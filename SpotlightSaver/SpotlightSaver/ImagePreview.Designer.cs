37eb2cd7b32994a89b97a67f60bbc1f9a96a9ca4
Mark Stott <mypantsareontheoutside@gmail.com>
2016-01-22 13:02:51 +0000

    Save it

--- SpotlightSaver/SpotlightSaver/ImagePreview.Designer.cs
+++ SpotlightSaver/SpotlightSaver/ImagePreview.Designer.cs
@@ -29,17 +29,46 @@
         private void InitializeComponent()
         {
             this.pbPreview = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.pbPreview)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pbPreview
             // 
             this.pbPreview.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pbPreview.Location = new System.Drawing.Point(0, 0);
+            this.pbPreview.Location = new System.Drawing.Point(0, 24);
             this.pbPreview.Name = "pbPreview";
-            this.pbPreview.Size = new System.Drawing.Size(284, 261);
+            this.pbPreview.Size = new System.Drawing.Size(284, 237);
             this.pbPreview.TabIndex = 0;
             this.pbPreview.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "&Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
             // ImagePreview
             // 
@@ -47,15 +76,24 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.pbPreview);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "ImagePreview";
             this.Text = "ImagePreview";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.pbPreview)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pbPreview;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
     }
 }
--- SpotlightSaver/SpotlightSaver/ImagePreview.cs
+++ SpotlightSaver/SpotlightSaver/ImagePreview.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
 using SpotlightSaver.Domain;
 
 namespace SpotlightSaver
@@ -17,6 +19,38 @@ namespace SpotlightSaver
             InitializeComponent();
 
             pbPreview.Load(imagePath);
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var fileInfo = new FileInfo(pbPreview.ImageLocation);
+            if (!fileInfo.Exists) return;
+
+            var saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "JPEG File (*.jpg)|*.jpg|All files (*.*)|*.*";
+            saveDialog.CheckFileExists = false;
+            saveDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            saveDialog.DefaultExt = "jpg";
+            saveDialog.AddExtension = true;
+            saveDialog.Title = "Save Spotlight Image";
+
+            if (saveDialog.ShowDialog() == DialogResult.OK) SaveFile(saveDialog.FileName);
+        }
+
+        private void SaveFile(string fileName)
+        {
+            var existingFile = new FileInfo(pbPreview.ImageLocation);
+            var newFile = new FileInfo(fileName);
+
+            if (newFile.Exists)
+            {
+                if (MessageBox.Show("An image by the same name already exists, do you want to replace it?", "Confirmation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    newFile.Delete();
+                else
+                    return;
+            }
+
+            existingFile.CopyTo(fileName);
         }
     }
 }
